27d7063ae67fc4aaff42627f0f19d2989fda11f7
Selin Ağan <selinbasakagan@gmail.com>
2022-06-07 11:43:16 +0300

    navigation changes

--- app/src/main/java/com/example/deneme/After_Login.kt
+++ app/src/main/java/com/example/deneme/After_Login.kt
@@ -3,6 +3,9 @@ package com.example.deneme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_after_login.*
 
 class After_Login : AppCompatActivity() {
@@ -15,6 +18,18 @@ class After_Login : AppCompatActivity() {
 
         //val take_message2 = intent.getStringExtra("namekey")
         //afterlogin_username.text = take_message2
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navhost_fragment_afterlogin) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomnav, navHostFragment.navController)
+
+        NavigationUI.setupWithNavController(navigationView, navHostFragment.navController)
+
+        toolbar.title = "SHOPIE"
+        val toggle = ActionBarDrawerToggle(this,drawer,toolbar, 0,0)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
 
 
 
